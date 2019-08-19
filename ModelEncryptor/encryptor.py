f804fdc56f6d410ac1a839e3e3f1357656ac323a
ayivima <48770118+ayivima@users.noreply.github.com>
2019-08-19 13:20:09 +0000

    Create encryptor.py

---
+++ ModelEncryptor/encryptor.py
@@ -0,0 +1,51 @@
+
+
+import syft
+import torch
+
+
+class ModelEncryptor():
+
+  def __init__(self, model, num_of_shares):
+
+    hook = syft.TorchHook(torch)
+
+    # Function for generating a share
+    share = lambda share_id: (
+      sy.VirtualWorker(hook, id=share_id).add_worker(sy.local_worker)
+    )
+
+    # Generate ids for shares based on number of shares
+    share_ids = [
+      "".join(["share", str(num + 1)]) 
+      for num in range(num_of_shares + 1)
+    ]
+
+    # Generate shares based on number of shares specified
+    self.shares = list(map(share, share_ids))
+    
+    # Encrypt model
+    self.model = (
+      model.fix_precision().share(
+        *self.shares[:-1], crypto_provider=self.shares[-1]
+      )
+    )
+
+  def encrypt_data(self, data):
+    """Encrypts data."""
+    return (
+      data.fix_precision().share(
+      *self.shares[:-1], crypto_provider=self.shares[-1]
+    )
+  )
+
+  def predict(self, data):
+    """Encrypts data, and returns the outcome of 
+    an encrypted prediction or classification.
+    
+    """
+    e_data = self.encrypt_data(data)
+
+    return self.model(e_data).get().float_precision()
+
+
